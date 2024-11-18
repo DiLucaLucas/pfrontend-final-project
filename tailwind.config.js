509400b2855b814ff88a5b29923c7fb092d48ac4
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-18 19:30:31 -0300

    `Feat: Global Styles` Se añadieron las clases de estilos globales personalizados y tipografia.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "success-green": "#20CA82",
+        "primary-green": "#4BFFB3",
+        "secondary-blue": "#4B7DFF",
+        "text-white": "#FFFFFF",
+        "text-gray": "#D9D9D9",
+        "input-gray": "#808080",
+        "card-gray": "#545454",
+        "background-gray": "#3D3D3D",
+        "almost-black": "#222222",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
