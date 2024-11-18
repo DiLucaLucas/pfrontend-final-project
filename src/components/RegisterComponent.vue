bc6fde89cb8dfa99890e8ffeb71abb690dbcf5dc
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-18 19:31:01 -0300

    ``Feat: RegisterComponent` Se creo el componente de Registro

---
+++ src/components/RegisterComponent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+const confPassword = ref('');
+
+const onRegister = () => {
+  console.log('Registro exitoso');
+}
+</script>
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="w-full max-w-md p-8 bg-card-gray rounded-lg shadow-lg">
+      <h2 class="text-2xl font-bold text-center text-primary-green py-2">Registrate</h2>
+      <p class="text-center text-text-gray mb-8 text-md">
+          Registra tus datos para acceder a tu cuenta. ✍️
+        </p>
+      <form @submit.prevent="onRegister">
+        <input type="email" placeholder="Correo Electronico" v-model="email" class="w-full px-4 py-3 mb-6 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <input type="password" placeholder="Contraseña" v-model="password" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <input type="password" placeholder="Confirmar Contraseña" v-model="confPassword" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <button type="submit" class="w-full py-2 mb-6 text-white bg-success-green font-bold rounded-md hover:bg-primary-green hover:text-gray-950 transition-colors"> 🔐 Ingresar</button>
+        <p class="mt-4 text-center text-text-gray">
+          ¿Ya tienes cuenta?
+            <button @click="$emit('switchToLogin')" class="text-secondary-blue hover:text-blue-800">
+              Inicia Sesion 📝
+            </button>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
