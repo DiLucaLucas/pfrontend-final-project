51317b6b1eaddce6883a011fdab5c3673a29e57d
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-19 12:57:54 -0300

    `Fix: RegisterComponent` | Se ajusto formulario.

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -10,23 +10,17 @@ const onRegister = () => {
 }
 </script>
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="w-full max-w-md p-8 bg-card-gray rounded-lg shadow-lg">
+  <div class="flex items-center justify-center mt-10">
+    <div class="w-full max-w-md p-8 bg-card-gray rounded-lg">
       <h2 class="text-2xl font-bold text-center text-primary-green py-2">Registrate</h2>
       <p class="text-center text-text-gray mb-8 text-md">
-          Registra tus datos para acceder a tu cuenta. ✍️
+          Registra tus datos para acceder a tu cuenta.✍️
         </p>
       <form @submit.prevent="onRegister">
         <input type="email" placeholder="Correo Electronico" v-model="email" class="w-full px-4 py-3 mb-6 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
         <input type="password" placeholder="Contraseña" v-model="password" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
         <input type="password" placeholder="Confirmar Contraseña" v-model="confPassword" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
-        <button type="submit" class="w-full py-2 mb-6 text-white bg-success-green font-bold rounded-md hover:bg-primary-green hover:text-gray-950 transition-colors"> 🔐 Ingresar</button>
-        <p class="mt-4 text-center text-text-gray">
-          ¿Ya tienes cuenta?
-            <button @click="$emit('switchToLogin')" class="text-secondary-blue hover:text-blue-800">
-              Inicia Sesion 📝
-            </button>
-        </p>
+        <button type="submit" class="w-full py-2 mb-6 text-white bg-success-green font-bold rounded-md hover:bg-primary-green hover:text-gray-950 transition-colors">Ingresar 🔐</button>
       </form>
     </div>
   </div>
