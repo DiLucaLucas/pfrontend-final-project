bc3a943ee37dc226e6013543b1418449156a6417
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-19 22:58:35 -0300

    `Feat: RegisterComponent` | Utilizamos el store de registro.

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+// Importamos reactive y ref
+// Importamos nuestro store
+import { reactive, ref } from 'vue';
+import { useSesionStore } from '@/stores/auth_session.store'
 
-const email = ref('');
-const password = ref('');
-const confPassword = ref('');
+// Importamos el modelo
+import type { CredencialesModel } from '@/models/credencial.model';
 
-const onRegister = () => {
-  console.log('Registro exitoso');
+// Hacemos reactivas nuestras constantes
+const sesionStore = useSesionStore()
+const auth = reactive(sesionStore)
+
+// Definimos la estructura de nuestra constante
+const credenciales : CredencialesModel = {
+  email: '',
+  password: '',
+}
+
+// Creamos una referencia individual
+const input_confirm : string = '';
+const inp_conf = ref(input_confirm);
+
+// Convertimos nuestras credenciales reactivas
+const credencialesReactivas = reactive(credenciales);
+
+async function VerificarCredenciales() {
+  // Comparamos los valores de la credencial reactiva y la confirmacion de la contraseña
+  if (credencialesReactivas.password === inp_conf.value) {
+    await auth.registrarUsuario(credenciales)
+  } else {
+    alert('Las contraseñas no coinciden ❌⛔')
+  }
 }
 </script>
 <template>
@@ -16,10 +40,11 @@ const onRegister = () => {
       <p class="text-center text-text-gray mb-8 text-md">
           Registra tus datos para acceder a tu cuenta.✍️
         </p>
-      <form @submit.prevent="onRegister">
-        <input type="email" placeholder="Correo Electronico" v-model="email" class="w-full px-4 py-3 mb-6 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
-        <input type="password" placeholder="Contraseña" v-model="password" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
-        <input type="password" placeholder="Confirmar Contraseña" v-model="confPassword" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <!-- Utilizamos la funcion, para que se dispare, al hacer click -->
+        <form @submit.prevent="VerificarCredenciales()">
+        <input type="email" placeholder="Correo Electronico" v-model="credencialesReactivas.email" class="w-full px-4 py-3 mb-6 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <input type="password" placeholder="Contraseña" v-model="credencialesReactivas.password" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
+        <input type="password" placeholder="Confirmar Contraseña" v-model="inp_conf" class="w-full px-4 py-3 mb-8 text-input-gray bg-almost-black rounded-md focus:outline-none focus:ring focus:ring-secondary-blue">
         <button type="submit" class="w-full py-2 mb-6 text-white bg-success-green font-bold rounded-md hover:bg-primary-green hover:text-gray-950 transition-colors">Ingresar 🔐</button>
       </form>
     </div>
