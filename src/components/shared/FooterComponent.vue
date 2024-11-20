9d06e265ef08e96b3c7179fa393c309bd8fa6c2f
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-19 21:48:53 -0300

    `Feat: FooterComponent` | Se añadio componente de Footer. - Con nombre de integrantes. - Link a Repositorio de Github

---
+++ src/components/shared/FooterComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts"></script>
+<template>
+  <footer
+    class="bg-background-gray w-full text-gray-400 text-center p-6 absolute"
+  >
+    <h3>
+      Desarrollado por<span>Jeronimo Huincaman</span> &
+      <span>Lucas Di Luca</span>
+    </h3>
+    <a
+      href="https://github.com/DiLucaLucas/pfrontend-final-project.git"
+      class="text-primary-green ml-2"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Repositorio
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-10 w-full"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12 2C6.477 2 2 6.582 2 12.063c0 4.426 2.865 8.181 6.84 9.504.5.092.682-.222.682-.495 0-.244-.009-.889-.014-1.745-2.782.61-3.37-1.37-3.37-1.37-.454-1.166-1.11-1.477-1.11-1.477-.908-.635.068-.622.068-.622 1.004.072 1.533 1.041 1.533 1.041.892 1.556 2.34 1.107 2.91.847.092-.662.35-1.107.637-1.362-2.22-.258-4.556-1.136-4.556-5.056 0-1.117.39-2.031 1.029-2.748-.103-.256-.446-1.287.099-2.683 0 0 .84-.27 2.75 1.025A9.434 9.434 0 0 1 12 6.843c.85.004 1.705.116 2.504.34 1.91-1.296 2.748-1.025 2.748-1.025.546 1.396.203 2.427.1 2.683.64.717 1.027 1.631 1.027 2.748 0 3.929-2.34 4.793-4.567 5.047.359.316.68.939.68 1.894 0 1.366-.012 2.467-.012 2.8 0 .275.18.592.688.492A10.065 10.065 0 0 0 22 12.063C22 6.582 17.523 2 12 2z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </a>
+  </footer>
+</template>
+<style></style>
