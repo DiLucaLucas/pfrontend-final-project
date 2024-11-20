72d000ca9827990b4b6d8774d7ce2ecb852481e8
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2024-11-20 00:15:12 -0300

    `Feat: Tareas: ` | CRUD de tareas, implementacion de store. - Se implementa store

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,52 +1,65 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { reactive } from 'vue';
 
-// Estado para las tareas
-const tasks = ref<string[]>([]); // Lista de tareas
-const newTask = ref(''); // Texto del input
+//Importamos store de auth
+import { useSesionStore } from '@/stores/auth_session.store';
 
-// Función para agregar tareas
-const addTask = () => {
-  if (newTask.value.trim()) {
-    tasks.value.push(newTask.value.trim()); // Agrega la tarea a la lista
-    newTask.value = ''; // Limpia el input
-  }
-};
+// Importamos store de tarea
+import { useTareaStore } from '@/stores/tarea.store';
 
-// Función para eliminar tareas
-const removeTask = (index: number) => {
-  tasks.value.splice(index, 1); // Elimina la tarea por índice
-};
+// Importamos modelo de tarea
+import type { Tarea } from '@/models/Tarea';
+
+// Instanciamos el store de auth
+const sesionStore = useSesionStore();
+const auth = reactive(sesionStore);
+
+// Instanciamos el store de tareas
+const tareaStore = useTareaStore();
+const tareaSt = reactive(tareaStore);
+
+
+const nueva_tarea: Tarea = {
+  id: 0,
+  tarea: '',
+  completada: false
+}
+
+const tarea_reactiva = reactive(nueva_tarea);
+
+function completarTarea() {
+  tarea_reactiva.completada = !tarea_reactiva.completada;
+}
+
+function guardarTarea() {
+  tareaSt.agregarTarea(tarea_reactiva);
+}
+
+function eliminarTarea(tarea: Tarea) {
+  tareaSt.eliminarTarea(tarea)
+}
+
+// Listamos las tareas
+tareaSt.getTareas();
+
 </script>
 <template>
   <div class="min-h-screen bg-almost-black flex flex-col p-6">
     <h1 class="text-2xl text-success-green font-bold mb-4">Mis Tareas</h1>
     <div class="w-full max-lw-lg flex gap-2 mb-6">
-      <input
-        type="text"
-        v-model="newTask"
-        placeholder="Nueva tarea...."
-        class="flex-1 p-3 rounded-md bg-background-gray text-white "
-      />
-      <button
-        @click="addTask"
-        class="p-3 bg-success-green hover:text-gray-950 transition-colors rounded-md shadow font-bold flex items-center justify-center"
-      >
+      <input type="text" v-model="tarea_reactiva.tarea" placeholder="Nueva tarea...."
+        class="flex-1 p-3 rounded-md bg-background-gray text-white " />
+      <button @click="guardarTarea()"
+        class="p-3 bg-success-green hover:text-gray-950 transition-colors rounded-md shadow font-bold flex items-center justify-center">
         <img src="../assets/icons/add-icon.png" alt="" />
       </button>
     </div>
     <!-- Tareas -->
     <div class="w-full  space-y-4">
-      <div
-        v-for="(task, index) in tasks"
-        :key="index"
-        class="flex items-center justify-between p-4 rounded-md bg-white"
-      >
-        <span class="text-black font-bold">{{ task }}</span>
-        <button
-          @click="removeTask(index)"
-          class="hover:text-red-700"
-        >
+      <div v-for="tarea in tareaSt.data" :key="tarea.id"
+        class="flex items-center justify-between p-4 rounded-md bg-white">
+        <span class="text-black font-bold">{{ tarea.tarea }}</span>
+        <button @click="eliminarTarea(tarea)" class="hover:text-red-700">
           <img src="../assets/icons/trash-icon.png" alt="" />
         </button>
       </div>
