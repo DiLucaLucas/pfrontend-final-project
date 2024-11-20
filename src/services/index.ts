a8cbb9c5b4d02bc56996d6a538e54593b9015fc8
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2024-11-19 21:44:32 -0300

    `Feat: API` | Encapsulamos los metodos del controller tareas.

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,15 @@
 import { crearUsuario, login, logout } from "./auth/auth.controller";
+import { crearTarea, eliminarTarea, getTareas, editarTarea } from "./tareas/tareas.controller";
 
 export const API = {
     // Metodos de autenticacion
     login,
     crearUsuario,
-    logout
+    logout,
+
+
+    crearTarea,
+    editarTarea,
+    getTareas,
+    eliminarTarea
 }
